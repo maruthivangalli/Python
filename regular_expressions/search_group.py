e922c7c68cb188b45b33f5e915dc01f96f73d611
Nadeem-Ahmed <nadeemahmed@mirafra.com>
2021-09-30 11:59:43 +0530

    Added conditions to search_group file

--- regular_expressions/search_group.py
+++ regular_expressions/search_group.py
@@ -5,9 +5,12 @@
 import re
 
 match =re.search('(\d{3}) (\d{2})','39801 356 222 101' );
-print("Match Group Index 1 position "+match.group(1));
-print("Match Group Index 2 position "+match.group(2));
-print(match.groups());
-print(match.span());
-print(match.start());
-print(match.end());
+if(match):
+  print("Match Group Index 1 position "+match.group(1));
+  print("Match Group Index 2 position "+match.group(2));
+  print(match.groups());
+  print(match.span());
+  print(match.start());
+  print(match.end());
+else:
+  print("Match Not Found");
