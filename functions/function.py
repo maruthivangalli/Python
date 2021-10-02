8853bf6d3f4da877b30abb4c3203b497815b11fd
maruthivangalli <maruthivangalli@mirafra.com>
2021-10-02 11:40:01 +0530

    adding function.py file

---
+++ functions/function.py
@@ -0,0 +1,11 @@
+#Function defination
+def simple_func():
+  print("calling simple function");
+#Function calling
+simple_func()
+
+#function defining
+def simple(name):
+  print("my name is"+name);
+#Function calling
+simple("Maruthi")
